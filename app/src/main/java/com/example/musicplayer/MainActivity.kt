617977b4d7e6447adbeb69bc41bb897e140f12eb
kevinmartinez <ceronk003@gmail.com>
2023-03-22 14:59:15 -0600

    feat: adding exoplayer and MotionLayout dependencies

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -12,8 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private val songs = arrayOf("Bar Liar", "Girls Like You", "See You Again")
     private val songResources = intArrayOf(R.raw.song1, R.raw.song2, R.raw.song3)
-    private val albumArts =
-        intArrayOf(R.drawable.album_art_1, R.drawable.album_art_2, R.drawable.album_art_3)
+    private val albumArts = intArrayOf(R.drawable.album_art_1, R.drawable.album_art_2, R.drawable.album_art_3)
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         setupListView()
+
     }
-
-
     private fun initViews() {
         listview = binding.listView
     }
-
     private fun setupListView() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, songs)
         listview.adapter = adapter
