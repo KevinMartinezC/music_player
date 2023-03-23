244e1ae5cd7730b3b9be9cbd87f54ce8a7c6d36e
kevinmartinez <ceronk003@gmail.com>
2023-03-23 10:35:28 -0600

    feat: adding "song data class" to add the song details, and creating a companion object for const variables

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -6,13 +6,16 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.musicplayer.databinding.ActivityMainBinding
+import com.example.musicplayer.models.Song
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listview: ListView
 
-    private val songs = arrayOf("Bar Liar", "Girls Like You", "See You Again")
-    private val songResources = intArrayOf(R.raw.song1, R.raw.song2, R.raw.song3)
-    private val albumArts = intArrayOf(R.drawable.album_art_1, R.drawable.album_art_2, R.drawable.album_art_3)
+    private val songs = arrayOf(
+        Song(SONG_NAME_ONE, R.raw.song1, R.drawable.album_art_1),
+        Song(SONG_NAME_TWO, R.raw.song2, R.drawable.album_art_2),
+        Song(SONG_NAME_THREE, R.raw.song3, R.drawable.album_art_3)
+    )
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,5 +32,10 @@ class MainActivity : AppCompatActivity() {
     private fun setupListView() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, songs)
         listview.adapter = adapter
+    }
+    companion object{
+        const val SONG_NAME_ONE : String = "Bar Liar"
+        const val SONG_NAME_TWO : String = "Girls Like You"
+        const val SONG_NAME_THREE : String = "See You Again"
     }
 }
---
+++ app/src/main/java/com/example/musicplayer/models/Song.kt
@@ -0,0 +1,3 @@
+package com.example.musicplayer.models
+
+data class Song(val title: String, val resource: Int, val albumArt: Int)
